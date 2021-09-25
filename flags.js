10986db99f2879f067c970224762d1a0cc6ce2b1
madhanse29 <91239353+madhanse29@users.noreply.github.com>
2021-09-25 12:36:14 +0530

    Create flags.js

---
+++ flags.js
@@ -0,0 +1,15 @@
+const getBlogs = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET" , "https://restcountries.com/v3/all" );
+    
+    xhr.responseType = "json";
+
+    xhr.onload = () => {
+        const blogs = xhr.response;
+        for (var blog of blogs)
+           console.log(blog.flags[0]);
+    };
+
+    xhr.send();
+};
+getBlogs();
